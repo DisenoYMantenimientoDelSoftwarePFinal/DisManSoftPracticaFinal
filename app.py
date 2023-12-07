ec3040adc679b5e9ca3c32a08bdb851472e8b1b0
asm1028 <117651049+asm1028@users.noreply.github.com>
2023-12-07 17:09:16 +0100

    Registro y login de usuarios. Falta excepcion de errores

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-#from sqlalchemy import String, ForeignKey
-#from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_colum, relationship
-#from typing import List, Optional
+from flask import Flask, flash, redirect, render_template, request, session as flask_session, url_for
 from markupsafe import escape
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship
+from sqlalchemy.orm import mapped_column
+from typing import List, Optional
+from sqlalchemy import select
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30), unique=True)
+    password: Mapped[Optional[str]]
+
+
+from sqlalchemy import create_engine
+
+engine = create_engine("sqlite:///./baseDatos/githubExplorer.sqlite", echo=True)
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import Session
+
+
+
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -12,11 +36,27 @@ def home():
     return render_template('bienvenido.html')
 
 
-
-@app.route("/register")   
-def register():
+@app.get("/register")   
+def register_get():
     return render_template('register.html')
 
+@app.post("/register")   
+def register_post():
+    #TODO: sqlalchemy.exc.IntegrityError
+    if (len(request.form['username']) != 0 and len(request.form['password']) != 0):
+        with Session(engine) as session:
+            usuario = User(
+                    username=request.form['username'], 
+                    password=request.form['password'])
+            
+            session.add(usuario)
+            session.commit()
+            flask_session['logged_in_user'] = usuario.username
+            flash('Te has logueado satisfactoriamente')
+            return redirect(url_for('private'))
+    else:
+        flash('Usuario o contraseña no correcto')
+    return render_template('register.html')
 
 
 ADMIN_USERNAME = 'admin'
@@ -30,21 +70,32 @@ def login_get():
 def login_post():
     error = None
     username = ''
-    username = request.form['username']
-    password = request.form['password']
-    if authenticate(username, password):
-        session['logged_in_user'] = username
-        flash('Te has logueado satisfactoriamente')
-        return redirect(url_for('private'))
-    else:
-        error = 'Invalid username/password'
+    password = ''
+    with Session(engine) as session:
+            result = session.scalars(
+                select(
+                    User,
+                ).where(
+                    User.username == request.form['username']
+                ).where(
+                    User.password == request.form['password'])
+            )
+            usuario = result.first()
+            if usuario is not None:
+                flask_session['logged_in_user'] = usuario.username
+                flash('Te has logueado satisfactoriamente')
+                return redirect(url_for('private'))
+            else:
+                error = 'Invalid username/password'
     return render_template(
         'login.html', 
         error=error, 
         username = username
     )
 
-    
+#stmt = select(User).where(User.name.in_(["spongebob", "sandy"]))
+
+
 
 def authenticate(username, password):
     #TODO: Autenticar a un usuario con una base de datos
@@ -55,30 +106,21 @@ def authenticate(username, password):
 def page_not_found(error):
     return render_template('pagina_no_encontrada.html'), 404
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('pagina_no_encontrada.html'), 404
 
 @app.route('/private')
 def private():
-    if 'logged_in_user' not in session:
+    if 'logged_in_user' not in flask_session:
         return redirect(url_for('login_get'))
     return render_template('private.html')
 
 
 @app.route("/logout")
 def logout():
-    session.pop('logged_in_user',None)
+    flask_session.pop('logged_in_user',None)
     return redirect(url_for('login_get'))
 
 
-"""
-class Base(DeclarativeBase):
-    pass
 
-class User(Base):
-    __tablename__ = "user_account"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
-    addresses: Mapped[List["Address"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
-    )
-"""
